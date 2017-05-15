8dddd708d55aa4fa0a6d8bddef75aaa4c632c752
Eugenij Kizim <jackjunior98@gmail.com>
2017-05-15 11:39:35 +0300

    Exceptions added to class Set

--- REDworkout/src/com/TempPackage/Set.java
+++ REDworkout/src/com/TempPackage/Set.java
@@ -28,10 +28,23 @@ public class Set {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public void setWeight() {
+    public void setWeight(String weight) throws NumberFormatException {
+        try {
+            Double doubleWeight = Double.parseDouble(weight);
+            if (doubleWeight < 0 || doubleWeight > 2000)
+                throw new NumberFormatException("Invalid data");
+        } catch (NumberFormatException e) {
+            System.out.println("//error");
+        }
     }
 
-    public double getWeight() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public double getWeight() throws NoDataException {
+        NoDataExceptionSource exceptionSource = new NoDataExceptionSource();
+        try {
+            Double weight = exceptionSource.possibleWeightExceptionSource();
+        } catch (NoDataException exception)
+        {
+            //code to send message to user
+        }
     }
 }
--- REDworkout/src/com/TempPackage/Statistics.java
+++ REDworkout/src/com/TempPackage/Statistics.java
@@ -1,5 +1,7 @@
 package com.TempPackage;
 
+import java.util.Date;
+
 public class Statistics {
 
     private double weight_sum;
