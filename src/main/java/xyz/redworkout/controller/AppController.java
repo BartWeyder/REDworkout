a0adc4900a05e5fc37bff32e8d99149923555aba
Eugenij Kizim <jackjunior98@gmail.com>
2017-06-04 20:44:04 +0300

    controller dose not work

--- src/main/java/xyz/redworkout/controller/AppController.java
+++ src/main/java/xyz/redworkout/controller/AppController.java
@@ -59,116 +59,14 @@ public class AppController {
      * This method will list all existing users.
      */
     @RequestMapping(value = { "/", "/main" }, method = RequestMethod.GET)
-    public String listUsers(ModelMap model) {
+    public String about(ModelMap model) {
 
-        List<User> users = userService.findAllUsers();
-        model.addAttribute("users", users);
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "main";
+        //List<User> users = userService.findAllUsers();
+        //model.addAttribute("users", users);
+        //model.addAttribute("loggedinuser", getPrincipal());
+        return "about";
     }
 
-    /**
-     * This method will provide the medium to add a new user.
-     */
-    /*
-    @RequestMapping(value = { "/newuser" }, method = RequestMethod.GET)
-    public String newUser(ModelMap model) {
-        User user = new User();
-        model.addAttribute("user", user);
-        model.addAttribute("edit", false);
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registration";
-    }
-    */
-
-    /**
-     * This method will be called on form submission, handling POST request for
-     * saving user in database. It also validates the user input
-     */
-    /*
-    @RequestMapping(value = { "/newuser" }, method = RequestMethod.POST)
-    public String saveUser(@Valid User user, BindingResult result,
-                           ModelMap model) {
-
-        if (result.hasErrors()) {
-            return "registration";
-        }
-        */
-
-		/*
-		 * Preferred way to achieve uniqueness of field [sso] should be implementing custom @Unique annotation
-		 * and applying it on field [sso] of Model class [User].
-		 *
-		 * Below mentioned peace of code [if block] is to demonstrate that you can fill custom errors outside the validation
-		 * framework as well while still using internationalized messages.
-		 *
-		 */
-
-		/*
-        if(!userService.isUserEmailUnique(user.getId(), user.getEmail())){
-            FieldError ssoError =new FieldError("user","email",messageSource.getMessage("non.unique.email", new String[]{user.getEmail()}, Locale.getDefault()));
-            result.addError(ssoError);
-            return "registration";
-        }
-
-        userService.saveUser(user);
-
-        model.addAttribute("success", "User " + user.getName() + " "+ user.getSurname() + " registered successfully");
-        model.addAttribute("loggedinuser", getPrincipal());
-        //return "success";
-        return "registrationsuccess";
-    }
-    */
-
-    /**
-     * This method will provide the medium to update an existing user.
-     */
-
-    /*
-    @RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.GET)
-    public String editUser(@PathVariable String email, ModelMap model) {
-        User user = userService.findByEmail(email);
-        model.addAttribute("user", user);
-        model.addAttribute("edit", true);
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registration";
-    }
-
-    */
-
-    /**
-     * This method will be called on form submission, handling POST request for
-     * updating user in database. It also validates the user input
-     */
-
-    /*
-    @RequestMapping(value = { "/edit-user-{ssoId}" }, method = RequestMethod.POST)
-    public String updateUser(@Valid User user, BindingResult result,
-                             ModelMap model, @PathVariable String ssoId) {
-
-        if (result.hasErrors()) {
-            return "registration";
-        }
-*/
-		/*//Uncomment below 'if block' if you WANT TO ALLOW UPDATING SSO_ID in UI which is a unique key to a User.
-		if(!userService.isUserSSOUnique(user.getId(), user.getSsoId())){
-			FieldError ssoError =new FieldError("user","ssoId",messageSource.getMessage("non.unique.ssoId", new String[]{user.getSsoId()}, Locale.getDefault()));
-		    result.addError(ssoError);
-			return "registration";
-		}*/
-
-    /*
-        userService.updateUser(user);
-
-        model.addAttribute("success", "User " + user.getName() + " "+ user.getSurname() + " updated successfully");
-        model.addAttribute("loggedinuser", getPrincipal());
-        return "registrationsuccess";
-    }
-
-
-    /**
-     * This method will provide UserRole list to views
-     */
     @ModelAttribute("roles")
     public List<UserRole> initializeProfiles() {
         return userRoleService.findAll();
